54b28f16ed4cc15968430aa9b0aaad191abe4dbb
Dementic <taurusrh@gmail.com>
2014-08-07 16:57:36 +0200

    (almost) cross browser full screen

---
+++ fullscreen.js
@@ -0,0 +1,24 @@
+$('#fullscreen').on('click', function () {
+    var element = document.documentElement;
+    if (!$.root_.hasClass("full-screen")) {
+        $.root_.addClass("full-screen");
+        if (element.requestFullscreen) {
+            element.requestFullscreen();
+        } else if (element.mozRequestFullScreen) {
+            element.mozRequestFullScreen();
+        } else if (element.webkitRequestFullscreen) {
+            element.webkitRequestFullscreen();
+        } else if (element.msRequestFullscreen) {
+            element.msRequestFullscreen();
+        }
+    } else {
+        $.root_.removeClass("full-screen");
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) {
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) {
+            document.webkitExitFullscreen();
+        }
+    }
+});
